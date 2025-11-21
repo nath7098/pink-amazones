099ce68ebdd562be8684153d7f3579d54b8565cf
nath7098 <nathcouton@gmail.com>
2025-11-21 11:36:25 +0100

    :bug: fix city

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -2,7 +2,7 @@
   <div class="bg-gradient-to-b from-white to-pink-50 min-h-screen py-8">
     <!-- Contact information banner -->
     <div class="max-w-5xl mx-auto flex flex-wrap justify-center gap-x-10 gap-y-4 mb-8 bg-white rounded-lg shadow-md p-6">
-      <UTooltip text="5 Rue du Commerce 37230 Pernay, France">
+      <UTooltip text="3 allée des petites hérissières 37510 Ballan Miré, France">
         <UButton @click="navigateTo('https://maps.app.goo.gl/Phn344rXTgipe2Kd7', {external: true, open: {target: '_blank'}})" variant="link" class="flex items-center gap-x-2 shrink-0 transition-transform hover:scale-105">
           <UIcon name="i-heroicons-map-pin" class="w-6 h-6 text-pink-600"/>
           <div class="text-gray-800 font-medium">Pernay</div>
